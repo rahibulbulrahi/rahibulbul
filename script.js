07eb0e27d8e699d1158bc91f2ef32b87e7e8cba7
Rahi Bulbul <rahibulbul@outlook.com>
2022-06-23 17:05:15 +0600

    .

---
+++ script.js
@@ -0,0 +1,73 @@
+// Navbar Sticky
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
+  });
+});
+
+// Navbar Scroll slide-up
+// $(".scroll-up-btn").click(function () {
+//   $("html").animate({ scrollTop: 0 });
+//   // removing smooth scroll on slide-up button click
+//   $("html").css("scrollBehavior", "auto");
+// });
+// $(".navbar .menu li a").click(function () {
+//   // applying again smooth scroll on menu items click
+//   $("html").css("scrollBehavior", "smooth");
+// });
+
+// Active Class on Navbar
+$(document).ready(function () {
+  $(".navbar .menu li a").click(function () {
+    $(".navbar .menu li a").removeClass("active");
+    $(this).addClass("active");
+  });
+});
+
+// Typing text animation
+var typed = new Typed(".info-typing", {
+  strings: [
+    "Web Developer...",
+    "Designer..",
+    "Discord Server Developer..",
+    "Discord Bot Developer...",
+    "Blogger..",
+  ],
+  typeSpeed: 100,
+  loop: true,
+  backSpeed: 120,
+});
+
+// About typing text animation
+var typed = new Typed(".about-typing", {
+  strings: [
+    "Web Developer",
+    "Designer",
+    "Discord Server Developer",
+    "Discord Bot Developer",
+    "Blogger",
+    "An Introvert",
+    "and a person who likes to learn new things",
+  ],
+  typeSpeed: 100,
+  loop: true,
+  backSpeed: 60,
+});
+
+// Skills bar animation
+const skillsSection = document.querySelector(".skills-bar");
+const progressbar = document.querySelectorAll(".progress-bar");
+
+window.addEventListener("scroll", () => {
+  const skillsBar = skillsSection.getBoundingClientRect().top;
+  const skillsBarPosition = window.innerHeight - skillsBar;
+  if (skillsBarPosition < 900) {
+    progressbar.forEach((bar) => {
+      bar.style.width = bar.dataset.percentage;
+    });
+  }
+});
